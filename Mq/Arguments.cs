c900707335afd6348d12a69401ea8c05e19e5931
Luc <luc.nempont@gmail.com>
2015-09-03 15:05:26 +0200

    Synchronization

---
+++ Mq/Arguments.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mq
+{
+    public enum ToolMode
+    {
+        NoSet,
+        Import,
+        Export
+    }
+
+    /// <summary>
+    /// Represents arguments of command
+    /// </summary>
+    class Arguments
+    {
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// <param name="args"></param>
+        public Arguments(string[] args)
+        {
+            CheckArguments(args);
+        }
+
+        /// <summary>
+        /// Checks validity of arguments
+        /// </summary>
+        /// <param name="args"></param>
+        private void CheckArguments(string[] args)
+        {
+            this.Max = Int32.MaxValue;
+            List<string> files = new List<string>();
+
+            for (int index = 0; index < args.Count(); index++)
+            {
+                string argument = args[index];
+
+                switch (argument)
+                {
+                    // Transactional
+                    case "-t":
+                        IsTransactional = true;
+                        break;
+                    case "-e":
+                        if (index + 2 >= args.Count())
+                        {
+                            throw new ArgumentException("Unexpected number of arguments");
+                        }
+                        else
+                        {
+                            if (Mode != ToolMode.NoSet)
+                            {
+                                throw new ArgumentException(string.Format("Mode is already defined. Argument : {0}", argument));
+                            }
+                            Mode = ToolMode.Export;
+                            ComputerName = args[index + 1];
+                            QueueName = args[index + 2];
+                            index += 2;
+                        }
+                        break;
+                    case "-i":
+                        if (index + 2 >= args.Count())
+                        {
+                            throw new ArgumentException("Unexpected number of arguments");
+                        }
+                        else
+                        {
+                            if (Mode != ToolMode.NoSet)
+                            {
+                                throw new ArgumentException(string.Format("Mode is already defined. Argument : {0}", argument));
+                            }
+                            Mode = ToolMode.Import;
+                            ComputerName = args[index + 1];
+                            QueueName = args[index + 2];
+                            index += 2;
+                        }
+                        break;
+                    case "-m":
+                    case "-max":
+                        {
+                            if (index + 1 >= args.Count())
+                            {
+                                throw new ArgumentException("Unexpected number of arguments for max value");
+                            }
+                            else
+                            {
+                                int maximum;
+                                if (!Int32.TryParse(args[index + 1], out maximum))
+                                {
+                                    throw new ArgumentException("Unexpected value for max value");
+                                }
+                                this.Max = maximum;
+                                index++;
+                            }
+                        }
+                        break;
+                    default:
+                        if (argument.StartsWith("-"))
+                            throw new ArgumentException(string.Format("Invalid argument : {0}", argument));
+                        else
+                            files.Add(argument);
+                        break;
+                }
+            }
+
+            if (this.Mode == ToolMode.NoSet)
+                throw new ArgumentException("Mode is unspecified");
+
+            this.Files = files.ToArray();
+        }
+
+        /// <summary>
+        /// Gets if transactional option is set
+        /// </summary>
+        public bool IsTransactional { get; private set; }
+
+        /// <summary>
+        /// Gets the computer name 
+        /// </summary>
+        public string ComputerName { get; private set; }
+
+        /// <summary>
+        /// Gets the queue name
+        /// </summary>
+        public string QueueName { get; private set; }
+
+        /// <summary>
+        /// Gets the mode selected (Import/Export)
+        /// </summary>
+        public ToolMode Mode { get; private set; }
+
+        /// <summary>
+        /// Gets the maximum number of messages to dequeue
+        /// </summary>
+        public int Max { get; private set; }
+
+        /// <summary>
+        /// Gets the list of files to import into msqueue
+        /// </summary>
+        public string[] Files { get; private set; }
+    }
+}
--- Mq/Program.cs
+++ Mq/Program.cs
@@ -1,15 +1,226 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Messaging;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Mq
 {
     class Program
     {
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args"></param>
+		static void Main(string[] args)
         {
+
+            if (args == null || args.Count() == 0)
+            {
+                HelpPage();
+                return;
+
+            }
+            Arguments options = null;
+
+            try
+            {
+                options = new Arguments(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                HelpPage();
+                return;
+            }
+
+            string qname = GetQueueFormat(options.ComputerName, options.QueueName);
+
+            if (options.Mode == ToolMode.Export)
+            {
+                // Export
+                ExportMessage(qname, options.IsTransactional, options.Max);
+            }
+            else if (options.Mode == ToolMode.Import)
+            {
+                // Import
+                ImportMessage(qname, options.IsTransactional, options.Files);
+            }
+        }
+
+        /// <summary>
+        /// Write the help page 
+        /// </summary>
+        static void HelpPage()
+        {
+            Console.WriteLine("Usage :");
+            Console.WriteLine("mq -[ei] computer queue [-t] [files] [-m maxValue] ");
+            Console.WriteLine("Options :");
+            Console.WriteLine("\t-e : export from msqueue");
+            Console.WriteLine("\t-i : import to msqueue");
+            Console.WriteLine("\t-m : Number max of message to export");
+            Console.WriteLine("\t[files] : Files to import into msqueue");
+        }
+
+        /// <summary>
+        /// Import specified files into specified queue
+        /// </summary>
+        /// <param name="qname"></param>
+        /// <param name="isTransactional"></param>
+        /// <param name="files"></param>
+        static void ImportMessage(string qname, bool isTransactional, IEnumerable<string> files)
+        {
+            MessageQueue queue = new MessageQueue(qname);
+            int count = 0;
+
+            foreach (string pattern in files)
+            {
+                string directory = Path.GetDirectoryName(pattern);
+                string searchPattern = Path.GetFileName(pattern);
+
+                if (string.IsNullOrEmpty(directory))
+                {
+                    directory = Directory.GetCurrentDirectory();
+                }
+
+                foreach (string file in Directory.GetFiles(directory, searchPattern))
+                {
+                    byte[] buffer = File.ReadAllBytes(file);
+
+                    Message m = new Message();
+                    m.BodyStream = new MemoryStream(buffer);
+                    m.BodyType = 768;
+                    if (!isTransactional)
+                    {
+                        queue.Send(m);
+                    }
+                    else
+                    {
+                        using (MessageQueueTransaction mqt = new MessageQueueTransaction())
+                        {
+                            mqt.Begin();
+                            queue.Send(m, mqt);
+                            mqt.Commit();
+                        }
+                    }
+                    count++;
+
+                    Console.WriteLine(file);
+                }
+            }
+            Console.WriteLine("Send " + count + " message(s) into " + qname);
+        }
+
+        /// <summary>
+        /// Export message from msqueue
+        /// </summary>
+        /// <param name="qname"></param>
+        /// <param name="isTransactional"></param>
+		static void ExportMessage(string qname, bool isTransactional, int maxValue)
+        {
+            string rep = Directory.GetCurrentDirectory();
+            MessageQueue queue = new MessageQueue(qname);
+            TimeSpan time = TimeSpan.FromMilliseconds(1000);
+
+            int count = 0;
+            try
+            {
+                do
+                {
+                    if (!isTransactional)
+                    {
+                        using (Message m = queue.Receive(time))
+                        {
+
+                            if (m.BodyStream != null)
+                            {
+                                string f = System.IO.Path.Combine(rep, Path.GetFileName(Path.GetTempFileName()));
+                                using (FileStream sw = File.Create(f))
+                                {
+                                    int l = 0; byte[] buffer = new byte[500];
+                                    do
+                                    {
+                                        l = m.BodyStream.Read(buffer, 0, 500);
+                                        if (l == 0) break;
+                                        sw.Write(buffer, 0, l);
+                                    } while (true);
+                                }
+                            }
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        using (MessageQueueTransaction mqt = new MessageQueueTransaction())
+                        {
+                            mqt.Begin();
+                            using (Message m = queue.Receive(time, mqt))
+                            {
+
+                                if (m.BodyStream != null)
+                                {
+                                    string f = System.IO.Path.Combine(rep, Path.GetFileName(Path.GetTempFileName()));
+                                    using (FileStream sw = File.Create(f))
+                                    {
+                                        int l = 0; byte[] buffer = new byte[500];
+                                        do
+                                        {
+                                            l = m.BodyStream.Read(buffer, 0, 500);
+                                            if (l == 0) break;
+                                            sw.Write(buffer, 0, l);
+                                        } while (true);
+                                    }
+                                }
+                                count++;
+                                mqt.Commit();
+                            }
+                        }
+                    }
+                } while (count < maxValue);
+            }
+            catch (System.Messaging.MessageQueueException e)
+            {
+                if (e.MessageQueueErrorCode != MessageQueueErrorCode.IOTimeout)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Retrieve " + count + " message(s)");
+        }
+
+        /// <summary>
+        /// Get the queue name formatted from machine and queue name specified
+        /// </summary>
+        /// <param name="machineName"></param>
+        /// <param name="queueName"></param>
+        /// <returns></returns>
+		private static string GetQueueFormat(string machineName, string queueName)
+        {
+            Regex regIpAddress = new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", RegexOptions.Compiled);
+
+            string retour = null;
+
+            if (!string.IsNullOrEmpty(machineName) && !string.IsNullOrEmpty(queueName))
+            {
+                if (machineName.Trim() == ".")
+                    retour = string.Format(@"{0}\Private$\{1}", machineName, queueName);
+                else if (regIpAddress.IsMatch(machineName))
+                    retour = string.Format("Formatname:DIRECT=TCP:{0}\\Private$\\{1}", machineName, queueName);
+                else
+                    retour = string.Format("Formatname:DIRECT=OS:{0}\\Private$\\{1}", machineName, queueName);
+            }
+
+            return retour;
         }
     }
 }
